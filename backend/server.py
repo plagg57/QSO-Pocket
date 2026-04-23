110f0b5bb4cfff4cf3145f55c0a18af75cc31a98
plagg57 <jojames60@hotmail.com>
2026-04-23 17:18:14 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -368,7 +368,8 @@ async def create_qso(qso_data: QSOCreate, request: Request):
 async def get_qsos_grouped(user_id: str, request: Request, search: Optional[str] = None, band: Optional[str] = None):
     user = await get_current_user(request)
     if user.get("role") != "admin":
-    raise HTTPException(status_code=403, detail="Accès refusé")
+    raise 
+    HTTPException(status_code=403, detail="Accès refusé")
     match_stage = {"owner_id": user_id}
     if search:
         match_stage["$or"] = [
